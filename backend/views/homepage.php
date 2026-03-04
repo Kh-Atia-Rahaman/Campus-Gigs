f966003289f3b223dd5a3e81a0a9d3f1a8e5ed0f
Kh-Atia-Rahaman <atiyahrahmanarin@gmail.com>
2026-03-04 08:40:00 +0600

    Refactor homepage view components

--- backend/views/homepage.php
+++ backend/views/homepage.php
@@ -93,3 +93,6 @@ $user_name = $is_logged_in ? ($_SESSION['user_name'] ?? 'User') : '';
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
+<!-- Homepage base template -->
+
